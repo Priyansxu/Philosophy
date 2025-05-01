9aee7294dd09a9117abd13a7fd7da5d2898bb432
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-05-01 18:08:44 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
   title: "PhiloQuotes",
