139f23fbf5e07bed9712412bde66f0b50d5f073d
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-05-01 16:11:47 +0530

    Create manifest.js

---
+++ app/manifest.js
@@ -0,0 +1,11 @@
+export default function manifest() {
+  return {
+    name: 'Philosophy Quotes', 
+    short_name: 'Philosophy Quotes',
+    description: '...',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#ffffff'
+  }
+}
