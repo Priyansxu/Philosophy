45b2f0576b76daff31e42bcf34b51d223f779cee
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-05-02 11:12:36 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -146,7 +146,7 @@ export default function QuotePage() {
       <div className="flex justify-center items-center gap-8 py-6 fixed bottom-2 w-full">
         <button
           onClick={prev}
-          className="flex items-center gap-2 px-5 py-2 border border-gray-300 hover:bg-gray-100 transition-colors rounded-l-xl bg-white/70  backdrop-blur-md"
+          className="flex items-center gap-2 px-5 py-2 border border-gray-300 hover:bg-gray-100 transition-colors rounded-l-xl bg-white/80  backdrop-blur-lg"
         >
           <ArrowLeft className="w-4 h-4 mr-2" />
         </button>
