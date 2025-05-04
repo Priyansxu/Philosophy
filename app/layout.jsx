74e65fb0d4f54de47ffede512fe33c86f1d7b870
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-05-04 22:26:38 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -38,18 +38,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth dark:bg-slate-700">
+    <html lang="en">
       <body className={`${montserrat.className} transition-colors duration-300`}>
-        <header className="border-b transition-colors duration-300 dark:border-slate-700 border-gray-200 dark:bg-slate-800 bg-white">
-          <div className="container mx-auto px-4">
-            <div className="flex h-16 items-center justify-center">
-              <h1 className="text-xl font-bold tracking-tight">
-                <span className="text-black opacity-75 dark:text-indigo-200">Philosophy</span>
-                <span className="text-gray-500 dark:text-indigo-400"> Quotes</span>
-              </h1>
-            </div>
-          </div>
-        </header>
         {children}
       </body>
     </html>
